8b81b846a4e581d7a40537205b29d6d9413f0f9b
igor.patreew <igor.patreew@activeplatform.com>
2020-03-20 12:51:15 +0300

    Initialize

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    # some code
+  end
+end
---
+++ spec/servises/example_test_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe '#method_name' do
+  let(:var)    { 1 }
+  let(:result) { 1 }
+
+  it 'returns accept result' do
+    expect(var).to eq(result)
+  end
+end
